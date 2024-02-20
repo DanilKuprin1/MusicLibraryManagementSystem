b485d2224a6a2e35b6c66101116792c4a07460c7
DanilKuprin1 <danilkuprinca@gmail.com>
2024-02-20 18:01:12 -0500

    Prettifying code

--- tests/song_test.py
+++ tests/song_test.py
@@ -4,5 +4,7 @@ from app.song import Song
 class TestSong:
 
     def test_print_song(self):
-        song = Song("Hello", "Adele", "Hello....")
+        song = Song("Hello", "Adele")
         assert str(song) == "Hello by Adele"
+
+    
